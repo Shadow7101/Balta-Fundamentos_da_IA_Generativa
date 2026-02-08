82e8d72a0ed3d6a877e3fd5bc393d54bdb5b5403
Wilson Junior <wilson.ti@hotmail.com.br>
2026-02-08 07:52:59 -0300

    ajustes

---
+++ 01-Ollama/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using OllamaSharp;
+
+
+var uri = new Uri("http://localhost:11434");
+var ollama = new OllamaApiClient(uri);
+
+var models = await ollama.ListLocalModelsAsync();
+
+foreach (var model in models)
+{
+    Console.WriteLine($"Model Name: {model.Name}");
+}
+
+
+ollama.SelectedModel = "phi3:latest";
+
+var chat = new Chat(ollama);
+
+
+while (true)
+{
+    Console.WriteLine("Digite sua mensagem (ou 'sair' para encerrar):");
+    var prompt = Console.ReadLine() ?? string.Empty;
+    
+    if (prompt.ToLower() == "sair")
+    {
+        break;
+    }
+
+    await foreach (var resposta in chat.SendAsync(prompt))
+    {
+        Console.Write(resposta);
+    }
+
+
+    Console.WriteLine("\n\nConversa encerrada.");
+    Console.ReadKey();
+    Console.Clear();
+}
